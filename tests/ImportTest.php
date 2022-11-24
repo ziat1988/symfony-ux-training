adeec0ae8df8b4f0066de51139c852146d15e63d
dang <trinhdangnhat@gmail.com>
2022-11-24 18:07:57 +0100

    set up test unit with place holder data

--- tests/ImportTest.php
+++ tests/ImportTest.php
@@ -1,9 +1,54 @@
 <?php declare(strict_types=1);
 
+use JetBrains\PhpStorm\NoReturn;
+
 class ImportTest extends \PHPUnit\Framework\TestCase
 {
-    public function testRandom():void
+
+    /**
+     * @dataProvider provideUri
+     * @param string $uri
+     * @return void
+     */
+    public function testImport(string $uri):void
     {
+        dump($uri);
         $this->assertSame(2,2,'same');
+    }
+
+
+    public function provideUri(): Generator
+    {
+        yield 'csv' => [__DIR__.'/Fixtures/food.csv'];
+        yield 'json' => [__DIR__.'/Fixtures/users.json'];
+    }
+
+
+
+    /**
+     * @dataProvider provideTrimData
+     */
+    public function testTrim($expectedResult, $input): void
+    {
+        dump($expectedResult);
+        dump($input);
+        self::assertSame($expectedResult, trim($input));
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function provideTrimData(): array
+    {
+        return [
+            [
+                'Hello World',
+                ' Hello World',
+            ],
+            [
+                'Hello World',
+                " Hello World \n",
+            ],
+        ];
     }
 }
